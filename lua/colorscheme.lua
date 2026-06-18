027e2f27fa4518abfd6d5446d42797485d105503
Davide Rinarelli <rinarellidavide@gmail.com>
2026-06-18 21:07:56 +0200

    fix: background tab color, with opacity
    Signed-off-by: Davide Rinarelli <rinarellidavide@gmail.com>

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -9,24 +9,31 @@ function M.apply(config, wezterm, nixInfo)
 
     local theme_file_path = theme_dir .. '/' .. theme_name .. '.toml'
     local scheme_colors = wezterm.color.load_scheme(theme_file_path)
-    local bg_color
+    local bg_color = '#000000'
     if scheme_colors and scheme_colors.background then
         bg_color = scheme_colors.background
     else
         wezterm.log_warn("Impossibile eseguire il parsing sincrono del file: " .. theme_file_path)
-        bg_color = 'none'
     end
+    local opacity = nixInfo(1.0, "opacity")
+    local r, g, b = 0, 0, 0
+    if bg_color:sub(1, 1) == '#' then
+        r = tonumber(bg_color:sub(2, 3), 16)
+        g = tonumber(bg_color:sub(4, 5), 16)
+        b = tonumber(bg_color:sub(6, 7), 16)
+    end
+    local rgba_bg_color = string.format("rgba(%d, %d, %d, %f)", r, g, b, opacity)
 
     config.colors = {
         tab_bar = {
-            background = bg_color, -- TODO: si puo mettere trasparente?
+            background = rgba_bg_color,
             active_tab = {
                 bg_color = '#000000',
                 fg_color = '#ffffff',
                 intensity = 'Bold',
             },
             inactive_tab = {
-                bg_color = bg_color,
+                bg_color = rgba_bg_color,
                 fg_color = '#808080',
             },
             inactive_tab_hover = {
@@ -34,7 +41,7 @@ function M.apply(config, wezterm, nixInfo)
                 fg_color = '#f0f0f0',
             },
             new_tab = {
-                bg_color = bg_color,
+                bg_color = rgba_bg_color,
                 fg_color = '#808080',
             },
             new_tab_hover = {
@@ -50,7 +57,7 @@ function M.apply(config, wezterm, nixInfo)
         local status = " NixOS - " .. username .. " "
         window:set_right_status(
             wezterm.format({
-                { Background = { Color = bg_color } },
+                { Background = { Color = rgba_bg_color } },
                 { Foreground = { Color = '#808080' } },
                 { Text = status },
             })
