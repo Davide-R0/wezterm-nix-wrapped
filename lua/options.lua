2169df79f4b570877f4d9c5837dabf7e7bc6e533
Davide Rinarelli <rinarellidavide@gmail.com>
2026-06-18 20:54:12 +0200

    fix: now options works and are well integratd.
    Signed-off-by: Davide Rinarelli <rinarellidavide@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -1,11 +1,14 @@
 local M = {}
 
 function M.apply(config, action, wezterm, nixInfo)
-    -- Font
-    config.font = wezterm.font(nixInfo("Hack Nerd Font", "fontFamily"))
+    local myConfig = {
+        -- Theme configs
+        font = wezterm.font(nixInfo("Hack Nerd Font", "fontFamily")),
+        font_size = nixInfo(14.0, "fontSize"),
+        cursor_blink_rate = 500,
+        default_cursor_style = 'BlinkingBlock',
 
-    -- Window configs
-    config = {
+        -- Window configs
         window_decorations = "NONE",
         window_background_opacity = nixInfo(0.9, "opacity"),
         window_close_confirmation = 'NeverPrompt',
@@ -16,36 +19,34 @@ function M.apply(config, action, wezterm, nixInfo)
             bottom = "10pt",
         },
 
-        -- Theme configs
-        --
-        font_size = nixInfo(15.0, "fontSize"),
-        cursor_blink_rate = 500,
-        default_cursor_style = 'BlinkingBlock',
-
         -- Misc configs
         front_end = "OpenGL",
         audible_bell = 'Disabled',
         hide_mouse_cursor_when_typing = true,
         scrollback_lines = 5000,
+
+        mouse_bindings = {
+            {
+                event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+                mods = 'NONE',
+                action = action.ScrollByLine(-2),
+                alt_screen = false,
+            },
+            {
+                event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+                mods = 'NONE',
+                action = action.ScrollByLine(2),
+                alt_screen = false,
+            },
+        },
+
+        --enable_wayland = false
+        --animation_fps = 60
     }
 
-    --config.enable_wayland = false
-    --config.animation_fps = 60
-
-    config.mouse_bindings = {
-        {
-            event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-            mods = 'NONE',
-            action = action.ScrollByLine(-2),
-            alt_screen = false,
-        },
-        {
-            event = { Down = { streak = 1, button = { WheelDown = 1 } } },
-            mods = 'NONE',
-            action = action.ScrollByLine(2),
-            alt_screen = false,
-        },
-    }
+    for k, v in pairs(myConfig) do
+        config[k] = v
+    end
 end
 
 return M
